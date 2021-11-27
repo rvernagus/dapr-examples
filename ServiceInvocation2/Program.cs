8fdd513c0ab569634f860009f04d243b43da2a7c
Ray Vernagus <r.vernagus@gmail.com>
2021-11-26 22:50:48 -0500

    Clean up service invocation example

--- ServiceInvocation2/Program.cs
+++ ServiceInvocation2/Program.cs
@@ -3,10 +3,7 @@ using Microsoft.AspNetCore.Mvc;
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.MapPost("/greet", (HttpRequest request, Person person) =>
-{
-    return $"Hello, {person.Name}!";
-});
+app.MapPost("/greet", (Person person) => $"Hello, {person.Name}!");
 
 app.Run("http://localhost:3500");
 
